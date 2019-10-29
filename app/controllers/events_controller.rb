562bf5aac17f46ff50b80c5cc5c47874f55a540f
soragotosann <7297230@qq.com>
2019-10-29 18:40:23 +0800

    10.29 updat

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,45 @@
 class EventsController < ApplicationController
+  #event/index
+  #event
   def index
     @events = Event.all
   end
+
+  # event/new
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new( event_pararms )
+    @event.save
+    redirect_to :action => :index #HTTP 303
+  end
+
+  private def event_pararms
+    params.require(:event).permit(:name , :description)
+  end  
+  
+  #event/show/:id
+  def show 
+    @event = Event.find( params[:id] )
+  end
+
+  #event/edit/id
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_pararms)
+    redirect_to :action => :show, :id => @event
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to :action => :index
+  end
+
 end
